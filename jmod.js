cdd9e4d949ce5055fad1e9700906ea300180f5ad
jomshir98 <jomshir98@protonmail.com>
2020-11-26 15:50:36 +0100

    Jmod: tampermonkey header

--- jmod.js
+++ jmod.js
@@ -1,3 +1,17 @@
+// ==UserScript==
+// @name         Jmod - Bondage Club
+// @namespace    jmod
+// @version      1.0.0.0
+// @description  Jomshir's collection of changes and patches for Bondage Club
+// @author       jomshir98
+// @match        https://www.bondageprojects.elementfx.com/*/BondageClub/*
+// @match        https://www.bondageprojects.com/college/*/BondageClub/*
+// @homepage     https://jomshir98.github.io/testing/
+// @updateURL    https://jomshir98.github.io/testing/jmod.js
+// @run-at		 document-end
+// @grant        unsafeWindow
+// ==/UserScript==
+
 (function () {
 	"use strict";
 	const w = window;
